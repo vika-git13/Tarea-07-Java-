bb2c69df3c9e46ff9d6211aba7c6b1b331a868d7
vika-git13 <bogoslavskaya.vika@gmail.com>
2025-12-04 01:00:55 +0200

    Update Validar.java

--- utils/Validar.java
+++ utils/Validar.java
@@ -1,1 +1,16 @@
+package utils;
 
+/**
+ *
+ * @author 38097
+ */
+public class Validar {
+      public static boolean validarDni(String dni){
+        return dni.matches("^[0-9]{8}[A-Z]$");
+    }
+    
+    public static boolean validarIBAN(String IBAN){
+    return IBAN.matches("^ES[0-9]{22}$");
+    }
+    
+}
